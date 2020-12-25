47c525ee8625d852f5b53ec70617968796681637
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-25 22:26:38 +0300

    Create navbar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  b-navbar
+    template(slot="start")
+      b-navbar-item(tag="router-link" :to="{ path: '/' }") Home
+
+    template(slot="end")
+      b-navbar-item(tag="div")
+        button.button.is-primary(@click="changeUserProfileType") {{ changeTo }} Profile
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('account', ['userProfileType']),
+    changeTo() {
+      return this.userProfileType == 'student' ? 'Lecturer' : 'Student'
+    }
+  },
+  methods: {
+    ...mapActions('account', ['changeUserProfileType'])
+  }
+}
+</script>
+
+<style lang="scss"></style>
