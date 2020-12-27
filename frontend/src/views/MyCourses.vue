6025dc81c3add28b6b0d91b6c12097269e7d20ef
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-27 16:40:14 +0300

    Create temporary page to display courses

---
+++ frontend/src/views/MyCourses.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  div.columns.is-multiline
+    div.column.is-3(v-for="course in user.courses" :key="course._id")
+      div.card
+        div.card-image
+          figure.image.is-4by3
+            img(:src="course.thumbnailUrl" :alt="course.title")
+
+        div.card-content
+          div.content
+            p {{ course.title }}
+            br
+            div.btns
+              b-button.mr-1(tag="router-link" :to="{ path: `/edit-course/${course._id}` }" size="is-small" type="is-primary" icon-left="pen" outlined) Edit Course
+              b-button(@click="deleteCourse(course._id)" size="is-small" type="is-danger" icon-left="delete" outlined) Delete
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('account', ['user'])
+  },
+  methods: {
+    deleteCourse(id) {
+      console.log('Course with id: ', id)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
