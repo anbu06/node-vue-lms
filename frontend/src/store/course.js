9e38d74569959ad10cc9a23674a0eff0f9967a67
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-28 12:22:13 +0300

    Add addLesson action to course store module

--- frontend/src/store/course.js
+++ frontend/src/store/course.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const actions = {
   GET_ALL_COURSES: "getAllCourses",
   CREATE_COURSE: "createCourse",
-  ADD_CHAPTER: "addChapter"
+  ADD_CHAPTER: "addChapter",
+  ADD_LESSON: "addLesson"
 };
 
 const course = {
@@ -19,6 +20,10 @@ const course = {
     async [actions.ADD_CHAPTER](store, data) {
       console.log(data)
       axios.post(`course/${data.courseId}/chapter`, data.chapter)
+    },
+    async [actions.ADD_LESSON](store, data) {
+      console.log(data)
+      axios.post(`course/${data.courseId}/chapter/${data.chapterId}/lesson`, data.chapter)
     }
   }
 };
