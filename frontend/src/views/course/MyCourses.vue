06fe5f6c63f98c935cad7e8b4b3af2b52a716703
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-29 11:28:25 +0300

    Refactor my-courses view

--- frontend/src/views/course/MyCourses.vue
+++ frontend/src/views/course/MyCourses.vue
@@ -1,31 +1,33 @@
 <template lang="pug">
   div
     router-link(to="/add-course") Add Course
-    a-card(hoverable style="width: 300px" v-for="course in courses" :key="course._id")
+    a-card(hoverable style="width: 300px" v-for="course in user.createdCourses" :key="course._id")
       img(
         slot="cover"
         alt="Course Thumbnail"
         :src="course.thumbnail")
 
-      a-badge.category(v-for="(item, index) in course.categories" :key="index")
-        small {{ item }}
+      template.ant-card-actions(slot="actions")
+        a-tooltip(placement="bottom")
+          template(slot="title")
+            span Course Details
+          router-link(:to="`/course-info/${course._id}`")
+            a-icon(type="question-circle")
+
+        a-tooltip(placement="bottom")
+          template(slot="title")
+            span Delete Course
+          a-icon(type="delete")
+
       a-card-meta(:title="course.title" :description="course.description")
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapState } from "vuex";
 
 export default {
-  data() {
-    return {
-      courses: []
-    }
-  },
-  methods: {
-    ...mapActions("course", ["getMyCourses"])
-  },
-  async created() {
-    this.courses = await this.getMyCourses()
+  computed: {
+    ...mapState("account", ["user"])
   }
 };
 </script>
